2c3d51bb0b23ffde89bdd5ba12ec0a65b6997c24
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-09-20 13:38:50 +0200

    added method to find the position where the data starts in a multichannel aquisition file

--- plugy/data/pmt.py
+++ plugy/data/pmt.py
@@ -28,7 +28,7 @@ from dataclasses import dataclass
 class PmtData(object):
     input_file: pl.Path
 
-    def read_txt(self):
+    def read_txt(self) -> pd.DataFrame:
         """
         Reads input_file
         :return: pd.DataFrame containing the PMT data of all channels
@@ -36,17 +36,34 @@ class PmtData(object):
         if self.input_file.exists():
             if self.input_file.suffix == ".gz":
                 with gzip.open(self.input_file, "rt") as f:
-                    for idx, line in enumerate(f):
-                        if re.match(pattern=r"\t\d", string=line) is not None:
-                            break
+                    end_of_header = self.find_data(f)
 
-                    f.seek(0)
-                    data_frame = pd.read_csv(f, sep="\t", decimal=",", skiprows=idx, header=None).iloc[:, 1:]
-                    data_frame.columns = ["time", "green", "orange", "uv"]
+            elif self.input_file.suffix == ".txt":
+                with self.input_file.open("rt") as f:
+                    end_of_header = self.find_data(f)
+
+            data_frame = pd.read_csv(self.input_file, sep="\t", decimal=",", skiprows=end_of_header, header=None).iloc[:, 1:]
+            data_frame.columns = ["time", "green", "orange", "uv"]
 
             return data_frame
 
         else:
             raise FileNotFoundError(f"Input file ({self.input_file.absolute()}) does not exist! Check the path!")
 
+    @staticmethod
+    def find_data(file) -> int:
+        """
+        Finds the ending of the header in a multichannel acquisition output file.
+        Identifies data by its leading \t
+        :param file: File object
+        :return: Line number of the first data line
+        """
+        idx = -1
+        for idx, line in enumerate(file):
+            if re.match(pattern=r"\t\d", string=line) is not None:
+                break
 
+        assert idx > -1, "No lines detected in input_file! Check the contents of the file!"
+        assert idx < 50, f"Automatically detected header length exceeds 50 lines ({idx})"
+
+        return idx
