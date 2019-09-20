c398512b2bff2cf6501f3b5059eec42460c5ba4f
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-09-20 13:05:40 +0200

    added some error handling

--- plugy/data/pmt.py
+++ plugy/data/pmt.py
@@ -29,14 +29,24 @@ class PmtData(object):
     input_file: pl.Path
 
     def read_txt(self):
-        with gzip.open(self.input_file, "rt") as f:
-            for idx, line in enumerate(f):
-                if re.match(pattern=r"\t\d", string=line) is not None:
-                    break
+        """
+        Reads input_file
+        :return: pd.DataFrame containing the PMT data of all channels
+        """
+        if self.input_file.exists():
+            if self.input_file.suffix == ".gz":
+                with gzip.open(self.input_file, "rt") as f:
+                    for idx, line in enumerate(f):
+                        if re.match(pattern=r"\t\d", string=line) is not None:
+                            break
 
-            # data = np.genfromtxt(f, delimiter="\t", skip_header=idx)
-            f.seek(0)
-            data_frame = pd.read_csv(f, sep="\t", decimal=",", skiprows=idx, header=None).iloc[:, 1:]
-            data_frame.columns = ["time", "green", "orange", "uv"]
+                    f.seek(0)
+                    data_frame = pd.read_csv(f, sep="\t", decimal=",", skiprows=idx, header=None).iloc[:, 1:]
+                    data_frame.columns = ["time", "green", "orange", "uv"]
 
-        return data_frame
+            return data_frame
+
+        else:
+            raise FileNotFoundError(f"Input file ({self.input_file.absolute()}) does not exist! Check the path!")
+
+
