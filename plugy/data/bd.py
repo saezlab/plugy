27167286f42466df6a396eee021c12985f535365
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-09-27 15:18:13 +0200

    added read_input_file for ChannelMap

--- plugy/data/bd.py
+++ plugy/data/bd.py
@@ -14,22 +14,45 @@ See accompanying file LICENSE.txt or copy at
     http://www.gnu.org/licenses/gpl-3.0.html
 
 """
+import logging
+
 import pathlib as pl
 
 from dataclasses import dataclass
 
+module_logger = logging.getLogger("plugy.data.bd")
+
 
 @dataclass
-class ChannelMapping(object):
+class ChannelMap(object):
     input_file: pl.Path
 
     def __post_init__(self):
-        self.mapping = self.read_input_file()
+        module_logger.info(f"Creating ChannelMap object from file {self.input_file.absolute()}")
+        module_logger.debug(f"Configuration:")
+        for k, v in self.__dict__.items():
+            module_logger.debug(f"{k}: {v}")
+
+        self.map = self.read_input_file()
 
     def read_input_file(self):
+        module_logger.info(f"Reading file")
+        mapping = dict()
         with self.input_file.open("r") as f:
-            pass
+            for line in f:
+                k, v = line.strip().split(":")
 
+                try:
+                    k = int(k)
+                except ValueError:
+                    raise ValueError(f"Channel has to be an int you specified {k}")
+
+                if k not in range(9, 25):
+                    raise ValueError(f"Channel out of BD range (9-24) you specified channel {k}")
+
+                mapping[k] = v
+
+        return mapping
 
 
 class PlugSequence(object):
--- plugy/test/test_bd.py
+++ plugy/test/test_bd.py
@@ -14,17 +14,50 @@ See accompanying file LICENSE.txt or copy at
     http://www.gnu.org/licenses/gpl-3.0.html
 
 """
+import tempfile
 import unittest
+import collections
+
+import pathlib as pl
+
+from ..data.bd import ChannelMap
 
 
 class TestChannelMapping(unittest.TestCase):
     def setUp(self) -> None:
-        header = "9:CELLS\n10:SUBSTRATE\n11:FS\n12:FS\n13:Drug1"
-        drugs = [f"{i}:Drug{i}" for i in range(13,)]
-        # self.test_file_content =
+        # header = "9:CELLS\n10:SUBSTRATE\n11:FS\n12:FS"
+        # drugs = [f"{i}:Drug{i}" for i in range(13, 23)]
+        # barcodes = "23:BC Low\n24:BC High"
+        # self.test_file_content = "\n".join([header, "\n".join(drugs), barcodes])
+
+        self.test_mapping = {9: "CELLS", 10: "SUBSTRATE", 11: "FS", 12: "FS"}
+        for idx, i in enumerate(range(13, 23)):
+            self.test_mapping[i] = f"Drug {idx + 1}"
+        self.test_mapping[23] = "BCL"
+        self.test_mapping[24] = "BCH"
+
+        self.test_file_content = "\n".join([f"{k}:{v}" for k, v in self.test_mapping.items()])
 
     def test_read_input_file(self):
-        self.assertEqual(True, False)
+        with tempfile.NamedTemporaryFile(mode="w+t", suffix=".txt") as self.channel_file:
+            self.channel_file.write(self.test_file_content)
+            self.channel_file.seek(0)
+            self.channel_file_path = pl.Path(self.channel_file.name)
+
+            mapping = ChannelMap(input_file=self.channel_file_path)
+
+        self.assertEqual(self.test_mapping, mapping.map)
+
+    def test_read_broken_input_file(self):
+        with tempfile.NamedTemporaryFile(mode="w+t", suffix=".txt") as self.channel_file:
+            self.channel_file.write("1:Test")
+            self.channel_file.seek(0)
+            self.channel_file_path = pl.Path(self.channel_file.name)
+
+            with self.assertRaises(ValueError) as cm:
+                mapping = ChannelMap(self.channel_file_path)
+
+        self.assertEqual(cm.exception.args[0], f"Channel out of BD range (9-24) you specified channel 1")
 
 
 class TestPlugSequence(unittest.TestCase):
