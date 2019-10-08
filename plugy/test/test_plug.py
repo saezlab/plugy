e0d1f3c0a6df70864cabe0de89db4cd5fe5e9a0f
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-10-08 17:12:57 +0200

    fixed data generation for cycles and switched all tests to be run on cycle data

--- plugy/test/test_plug.py
+++ plugy/test/test_plug.py
@@ -51,7 +51,6 @@ class TestPlugData(unittest.TestCase):
         self.time = np.linspace(0, self.signal_length, self.signal_length * self.acquisition_rate)
 
         self.clean_data = self.clean_data.assign(time=self.time)
-        self.noisy_data = self.noisy_data.assign(time=self.time)
 
         # Create clean square wave with period 2 pi and with its first rising edge at pi
         # self.clean_data = self.clean_data.assign(green=(sig.square(self.clean_data.time + np.pi) + 1) / 2)
@@ -59,14 +58,38 @@ class TestPlugData(unittest.TestCase):
         self.clean_data = self.clean_data.assign(uv=self.clean_data.green)
         self.clean_data = self.clean_data.assign(orange=self.clean_data.green)
 
-        self.clean_data.loc[self.clean_data.time > 2, "uv"] = 0
+        self.clean_data.loc[self.clean_data.time > 4, "orange"] = 0
+        self.clean_data.loc[(self.clean_data.time < 4) | (self.clean_data.time > 6), "uv"] = 0
         self.clean_data.loc[(self.clean_data.time < 3) | (self.clean_data.time > 4), "green"] = 0
-        self.clean_data.loc[(self.clean_data.time < 3) | (self.clean_data.time > 6), "orange"] = 0
+        # self.clean_data.loc[(self.clean_data.time < 3) | (self.clean_data.time > 6), "orange"] = 0
+
+        repeats = 5
+        for _ in range(repeats):
+            try:
+                # noinspection PyUnboundLocalVariable
+                tmp_data = tmp_data.append(self.clean_data)
+            except UnboundLocalError:
+                tmp_data = self.clean_data
+
+        # noinspection PyUnboundLocalVariable
+        self.clean_data = tmp_data.reset_index(drop=True)
+        #     self.single_plug_data = self.single_plug_data.append(self.single_plug_data)
+        # self.single_plug_data = self.single_plug_data.reset_index(drop=True)
+
+        self.clean_data = self.clean_data.assign(time=np.linspace(0, self.signal_length * repeats, self.signal_length * repeats * self.acquisition_rate))
+
+        # End of cycle
+        self.clean_data.loc[(self.clean_data.time > 15) & (self.clean_data.time < 20), "orange"] = 0
+        self.clean_data.loc[(self.clean_data.time > 15) & (self.clean_data.time < 20), "green"] = 0
+
+        self.clean_data.loc[(self.clean_data.time > 15) & (self.clean_data.time < 16), "uv"] = 1
+        self.clean_data.loc[(self.clean_data.time > 17) & (self.clean_data.time < 18), "uv"] = 1
 
         self.clean_data = self.clean_data.assign(green=self.clean_data.green * 0.9)
         self.clean_data = self.clean_data.assign(orange=self.clean_data.orange * 0.8)
 
         # Filter the clean signal with a mean filter to get slightly rounded edges
+        self.noisy_data = self.noisy_data.assign(time=self.clean_data.time)
         self.noisy_data = self.noisy_data.assign(green=fil.convolve1d(input=self.clean_data.green, weights=np.array(np.repeat(1, self.filter_size))) / self.filter_size)
         self.noisy_data = self.noisy_data.assign(uv=fil.convolve1d(input=self.clean_data.uv, weights=np.array(np.repeat(1, self.filter_size))) / self.filter_size)
         self.noisy_data = self.noisy_data.assign(orange=fil.convolve1d(input=self.clean_data.orange, weights=np.array(np.repeat(1, self.filter_size))) / self.filter_size)
@@ -80,27 +103,22 @@ class TestPlugData(unittest.TestCase):
         self.noisy_data = self.noisy_data.assign(orange=self.noisy_data.orange + np.random.normal(scale=self.noise_sigma, size=len(self.noisy_data.orange)))
 
         # Generate ground truth DataFrame
-        self.single_plug_data = pd.DataFrame({"start_time": [1.0, 3.0, 5.0],
-                                              # "center_time": [1.5, 3.5, 5.5],
-                                              "end_time": [2.0, 4.0, 6.0],
-                                              # "bc_peak_max": [1.0, 0.0, 0.0],
-                                              "barcode_peak_median": [1.0, 0.0, 0.0],
-                                              # "bc_peak_mean": [1.0, 0.0, 0.0],
-                                              # "control_peak_max": [0.0, 0.8, 0.8],
-                                              "control_peak_median": [0.0, 0.8, 0.8],
-                                              # "control_peak_mean": [0.0, 0.8, 0.8],
-                                              # "cell_peak_max": [0.0, 0.9, 0.0],
-                                              "readout_peak_median": [0.0, 0.9, 0.0],
-                                              # "cell_peak_mean": [0.0, 0.9, 0.0],
-                                              "barcode": [True, False, False]})
+        # self.single_plug_data = pd.DataFrame({"start_time": [1.0, 3.0, 5.0],
+        #                                       # "center_time": [1.5, 3.5, 5.5],
+        #                                       "end_time": [2.0, 4.0, 6.0],
+        #                                       # "bc_peak_max": [1.0, 0.0, 0.0],
+        #                                       "barcode_peak_median": [0.0, 0.0, 1.0],
+        #                                       # "bc_peak_mean": [1.0, 0.0, 0.0],
+        #                                       # "control_peak_max": [0.0, 0.8, 0.8],
+        #                                       "control_peak_median": [0.8, 0.8, 0.0],
+        #                                       # "control_peak_mean": [0.0, 0.8, 0.8],
+        #                                       # "cell_peak_max": [0.0, 0.9, 0.0],
+        #                                       "readout_peak_median": [0.0, 0.9, 0.0],
+        #                                       # "cell_peak_mean": [0.0, 0.9, 0.0],
+        #                                       "barcode": [False, False, True]})
 
-        # for _ in range(10):
-        #     self.clean_data = self.clean_data.append(self.clean_data)
-        #     self.single_plug_data = self.single_plug_data.append(self.single_plug_data)
-        # self.single_plug_data = self.single_plug_data.reset_index(drop=True)
-
-        self.cycle_data = pd.DataFrame({"start_time": [1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0, 25.0, 27.0, 29.0],
-                                        "end_time": [2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0, 30.0],
+        self.cycle_data = pd.DataFrame({"start_time": [1.0, 3.0, 5.0, 8.0, 10.0, 12.0, 15.0, 17.0, 19.0, 22.0, 24.0, 26.0, 29.0, 31.0, 33.0],
+                                        "end_time": [2.0, 4.0, 6.0, 9.0, 11.0, 13.0, 16.0, 18.0, 20.0, 23.0, 25.0, 27.0, 30.0, 32.0, 34.0],
                                         "barcode_peak_median": [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
                                         "control_peak_median": [0.8, 0.8, 0.0, 0.8, 0.8, 0.0, 0.0, 0.0, 0.0, 0.8, 0.8, 0.0, 0.8, 0.8, 0.0],
                                         "readout_peak_median": [0.0, 0.9, 0.0, 0.0, 0.9, 0.0, 0.0, 0.0, 0.0, 0.0, 0.9, 0.0, 0.0, 0.9, 0.0],
@@ -109,7 +127,7 @@ class TestPlugData(unittest.TestCase):
 
     @unittest.skip
     def test_plot_test_data(self):
-        test_data_fig, test_data_ax = plt.subplots(1, 2, figsize=(20, 10))
+        test_data_fig, test_data_ax = plt.subplots(1, 2, figsize=(40, 10))
         test_data_ax[0].plot(self.clean_data.time, self.clean_data.green, color="green")
         test_data_ax[0].plot(self.clean_data.time, self.clean_data.uv, color="blue")
         test_data_ax[0].plot(self.clean_data.time, self.clean_data.orange, color="orange")
@@ -133,7 +151,7 @@ class TestPlugData(unittest.TestCase):
             # noinspection PyTypeChecker
             plug_data = plug.PlugData(pmt_data=pmt.PmtData(input_file=pl.Path("MOCK")), plug_sequence=None, channel_map=None, peak_min_distance=0.03)
 
-        pd_test.assert_frame_equal(self.single_plug_data.round(), plug_data.plug_df.round())
+        pd_test.assert_frame_equal(self.cycle_data.round(), plug_data.plug_df.round())
 
     # noinspection DuplicatedCode
     def test_plug_detect_noisy_data(self):
@@ -144,7 +162,7 @@ class TestPlugData(unittest.TestCase):
             # noinspection PyTypeChecker
             plug_data = plug.PlugData(pmt_data=pmt.PmtData(input_file=pl.Path("MOCK")), plug_sequence=None, channel_map=None, peak_min_distance=0.03)
 
-        pd_test.assert_frame_equal(self.single_plug_data.round(), plug_data.plug_df.round())
+        pd_test.assert_frame_equal(self.cycle_data.round(), plug_data.plug_df.round())
 
 
 if __name__ == '__main__':
