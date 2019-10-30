e543e60a6e60bea0880826d3c3d16d00c7ab3a7c
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-10-30 14:20:55 +0100

    mocked __post_init__ to only execute PlugExperiment.check_config to enable other methods to be called in __post_init__

--- plugy/test/test_exp.py
+++ plugy/test/test_exp.py
@@ -15,6 +15,7 @@ See accompanying file LICENSE.txt or copy at
 
 """
 import unittest
+import unittest.mock as mock
 import tempfile
 import logging
 import itertools
@@ -34,22 +35,23 @@ class TestPlugExperiment(unittest.TestCase):
         """
         Tests if filename checking is properly implemented
         """
-        with tempfile.NamedTemporaryFile(mode="w+t", suffix=".csv") as self.test_file:
-            test_file_path = pl.Path(self.test_file.name)
-            test_file_names = set(itertools.permutations([None, None, None, test_file_path, test_file_path, test_file_path], 3))
+        with mock.patch.object(target=PlugExperiment, attribute="__post_init__", new=PlugExperiment.check_config):
+            with tempfile.NamedTemporaryFile(mode="w+t", suffix=".csv") as self.test_file:
+                test_file_path = pl.Path(self.test_file.name)
+                test_file_names = set(itertools.permutations([None, None, None, test_file_path, test_file_path, test_file_path], 3))
 
-            for file_names in test_file_names:
-                with self.subTest():
-                    if None in file_names:
-                        with self.assertRaises(AssertionError) as cm:
-                            PlugExperiment(config=PlugyConfig(pmt_file=file_names[0], seq_file=file_names[1], channel_file=file_names[2]))
-                        self.assertEqual(cm.exception.args[0], "One or more file paths are not properly specified, see the log for more information!")
+                for file_names in test_file_names:
+                    with self.subTest():
+                        if None in file_names:
+                            with self.assertRaises(AssertionError) as cm:
+                                PlugExperiment(config=PlugyConfig(pmt_file=file_names[0], seq_file=file_names[1], channel_file=file_names[2]))
+                            self.assertEqual(cm.exception.args[0], "One or more file paths are not properly specified, see the log for more information!")
 
-                    else:
-                        try:
-                            PlugExperiment(config=PlugyConfig(pmt_file=file_names[0], seq_file=file_names[1], channel_file=file_names[2]))
-                        except AssertionError:
-                            self.fail("PlugExperiment raised an AssertionError even though it was supplied with proper file names")
+                        else:
+                            try:
+                                PlugExperiment(config=PlugyConfig(pmt_file=file_names[0], seq_file=file_names[1], channel_file=file_names[2]))
+                            except AssertionError:
+                                self.fail("PlugExperiment raised an AssertionError even though it was supplied with proper file names")
 
 
 if __name__ == '__main__':
