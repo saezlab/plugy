bdd66bc2b20e049ce85a2d7e8dca34d985cfdf09
Türei Dénes <turei.denes@gmail.com>
2020-01-28 20:46:08 +0100

    file headers: comment instead of string as we don't want these in the docs

--- plugy/exp.py
+++ plugy/exp.py
@@ -1,19 +1,26 @@
-"""
-Author      Nicolas Peschke
-Date        19.09.2019
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-This file is part of the `plugy` python module
+#
+# Author      Nicolas Peschke
+# Date        19.09.2019
+#
+# This file is part of the `plugy` python module
+#
+# Copyright
+# 2018-2020
+# EMBL, Heidelberg University
+#
+# File author(s): Dénes Türei (turei.denes@gmail.com)
+#                 Nicolas Peschke
+#
+# Distributed under the GPLv3 License.
+# See accompanying file LICENSE.txt or copy at
+#     http://www.gnu.org/licenses/gpl-3.0.html
+#
+# Webpage: https://github.com/saezlab/plugy
+#
 
-Copyright
-2018-2020
-EMBL, Heidelberg University
-File author(s): Dénes Türei (turei.denes@gmail.com)
-                Nicolas Peschke
-Distributed under the GPLv3 License.
-See accompanying file LICENSE.txt or copy at
-    http://www.gnu.org/licenses/gpl-3.0.html
-
-"""
 import sys
 import logging
 import pathlib as pl
--- plugy/misc.py
+++ plugy/misc.py
@@ -1,7 +1,25 @@
-"""
-Author      Nicolas Peschke
-Date        04.04.2019
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Author      Nicolas Peschke
+# Date        04.04.2019
+#
+# This file is part of the `plugy` python module
+#
+# Copyright
+# 2018-2020
+# EMBL, Heidelberg University
+#
+# File author(s): Dénes Türei (turei.denes@gmail.com)
+#                 Nicolas Peschke
+#
+# Distributed under the GPLv3 License.
+# See accompanying file LICENSE.txt or copy at
+#     http://www.gnu.org/licenses/gpl-3.0.html
+#
+# Webpage: https://github.com/saezlab/plugy
+#
 
 import time
 
