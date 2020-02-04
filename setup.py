9d44c326a9f6285adfa0b6abbc994af08a0cd24f
Nicolas Peschke <nicolas.peschke@embl.de>
2020-02-04 10:20:07 +0100

    fixed version string in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='plugy',
-    version='v0.3.0',
+    version='v0.4.0',
     packages=['plugy', 'plugy.data', 'plugy.test'],
     url='https://github.com/saezlab/plugy',
     license='GPLv3',
