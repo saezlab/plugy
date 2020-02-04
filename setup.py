d3760244f3f308e535e11bae06534e39a718be4d
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-02-04 09:25:28 +0100

    initial commit for setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='plugy',
+    version='v0.3.0',
+    packages=['plugy', 'plugy.data', 'plugy.test'],
+    url='https://github.com/saezlab/plugy',
+    license='GPLv3',
+    author='Dénes Türei & Nicolas Peschke',
+    author_email='turei.denes@gmail.com',
+    description='A package to handle plug microfluidics data'
+)
