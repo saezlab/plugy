7b813cd49d98996c8f297a9fcfe9c271c61d6b8b
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2020-02-17 12:14:27 +0100

    adapted FILE_CONTENT to use "." as decimal separator

--- plugy/test/test_pmt.py
+++ plugy/test/test_pmt.py
@@ -36,7 +36,7 @@ import pandas.util.testing as pd_test
 
 from ..data import pmt
 
-FILE_CONTENT = """LabVIEW Measurement
+FILE_CONTENT_COMMA = """LabVIEW Measurement
 Writer_Version\t2
 Reader_Version\t2
 Separator\tTab
@@ -66,6 +66,36 @@ X_Value\tUntitled\tUntitled 1\tUntitled 2\tUntitled 3\tComment
 \t0,000000\t0,055849\t0,033265\t0,050356
 """
 
+FILE_CONTENT_DOT = """LabVIEW Measurement
+Writer_Version\t2
+Reader_Version\t2
+Separator\tTab
+Decimal_Separator\t.
+Multi_Headings\tNo
+X_Columns\tNo
+Time_Pref\tRelative
+Operator\tEveline
+Date\t2019/07/22
+Time\t17:18:18
+***End_of_Header***
+
+Channels\t4
+Samples\t100\t100\t100\t100
+Date\t2019/07/22\t2019/07/22\t2019/07/22\t2019/07/22
+Time\t17:18:18\t17:18:18\t17:18:18\t17:18:18
+X_Dimension\tTime\tTime\tTime\tTime
+X0\t0.0000000000000000E+0\t0.0000000000000000E+0\t0.0000000000000000E+0\t0.0000000000000000E+0
+Delta_X\t1.000000\t1.000000\t1.000000\t1.000000
+***End_of_Header***
+X_Value\tUntitled\tUntitled 1\tUntitled 2\tUntitled 3\tComment
+\t0.000000\t0.055544\t0.032960\t0.071718
+\t0.000000\t0.054323\t0.032044\t0.049745
+\t0.000000\t0.055239\t0.032655\t0.050050
+\t0.000000\t0.053713\t0.031739\t0.049135
+\t0.000000\t0.055544\t0.032655\t0.048830
+\t0.000000\t0.055849\t0.033265\t0.050356
+"""
+
 logging.basicConfig(level = logging.DEBUG,
                     format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt = '%d.%m.%y %H:%M:%S')
@@ -87,7 +117,7 @@ class TestPmtData(unittest.TestCase):
         """
         with tempfile.NamedTemporaryFile(mode = "w+b", suffix = ".txt.gz", delete = True) as self.gz_file:
             with gzip.GzipFile(mode = "wb", fileobj = self.gz_file) as gz:
-                gz.write(FILE_CONTENT.encode())
+                gz.write(FILE_CONTENT_COMMA.encode())
             self.gz_file.seek(0)
             self.gz_file_path = pl.Path(self.gz_file.name)
 
@@ -100,7 +130,7 @@ class TestPmtData(unittest.TestCase):
         Checks if reading normal txt file returns the expected DataFrame
         """
         with tempfile.NamedTemporaryFile(mode = "w+t", suffix = ".txt", delete = True) as self.txt_file:
-            self.txt_file.write(FILE_CONTENT)
+            self.txt_file.write(FILE_CONTENT_COMMA)
             self.txt_file.seek(0)
             self.txt_file_path = pl.Path(self.txt_file.name)
 
