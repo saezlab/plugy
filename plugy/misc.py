b477865f21cbfa540799eff27343eed919f65dcb
Türei Dénes <turei.denes@gmail.com>
2020-09-22 22:58:19 +0200

    git hash label: "braille-kidney" -> "plugy"

--- plugy/misc.py
+++ plugy/misc.py
@@ -30,15 +30,31 @@ import numpy as np
 
 def add_git_hash_caption(fig: plt.Figure, offset: float = 0.8):
     """
-    Adds a caption with the current git hash in a human readable form based on the most recent tag.
-    If the repository contains changes that are not committed yet, the keyword "-dirty" is appended to the caption.
+    Adds a caption with the current git hash in a human readable form based
+    on the most recent tag.
+    If the repository contains changes that are not committed yet,
+    the keyword "-dirty" is appended to the caption.
+
     :param fig: matplotlib.pyplot.Figure to add the caption to.
-    :param offset: Determines how many millimeter the caption is offset from the bottom left corner of the figure.
+    :param offset: Determines how many millimeter the caption is offset
+        from the bottom left corner of the figure.
+
     :return: None
     """
-    sha = sp.run(["git", "describe", "--tags", "--long", "--dirty"], capture_output = True, text = True).stdout.strip()
+
+    sha = sp.run(
+        ['git', 'describe', '--tags', '--long', '--dirty'],
+        capture_output = True,
+        text = True
+    ).stdout.strip()
     rel_position = offset / (fig.get_size_inches() * 25.4)
-    fig.text(rel_position[0], rel_position[1], f"Created on {time.ctime()} with braille-kidney {sha}", fontsize = "x-small", fontweight = "light")
+    fig.text(
+        rel_position[0],
+        rel_position[1],
+        f'Created on {time.ctime()} with plugy {sha}',
+        fontsize = 'x-small',
+        fontweight = 'light',
+    )
 
 
 def plot_line(slope: float, intercept: float, axes: plt.Axes):
