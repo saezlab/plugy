654d3b46cbbb13873f74c0d12638a7fc6b420181
Türei Dénes <turei.denes@gmail.com>
2021-04-12 01:04:59 +0200

    fixed input file names test

--- plugy/test/test_exp.py
+++ plugy/test/test_exp.py
@@ -20,45 +20,90 @@
 #
 
 
+import os
 import unittest
-import unittest.mock as mock
 import tempfile
 import logging
-import itertools
+import shutil
 
 import pathlib as pl
 
 from ..exp import PlugExperiment
 from ..data.config import PlugyConfig
 
-logging.basicConfig(level = logging.DEBUG,
-                    format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt = '%d.%m.%y %H:%M:%S')
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt = '%d.%m.%y %H:%M:%S',
+)
 
 
 class TestPlugExperiment(unittest.TestCase):
-    def test_check_config_file_names(self):
-        """
-        Tests if filename checking is properly implemented
-        """
-        with mock.patch.object(target = PlugExperiment, attribute = "__post_init__", new = PlugExperiment.check_config):
-            with tempfile.NamedTemporaryFile(mode = "w+t", suffix = ".csv") as self.test_file:
-                test_file_path = pl.Path(self.test_file.name)
-                test_file_names = set(itertools.permutations([None, None, None, test_file_path, test_file_path, test_file_path], 3))
 
-                for file_names in test_file_names:
-                    with self.subTest():
-                        if None in file_names:
-                            with self.assertRaises(AssertionError) as cm:
-                                PlugExperiment(config = PlugyConfig(pmt_file = file_names[0], seq_file = file_names[1], channel_file = file_names[2]))
-                            self.assertEqual(cm.exception.args[0], "One or more file paths are not properly specified, see the log for more information!")
 
-                        else:
-                            try:
-                                PlugExperiment(config = PlugyConfig(pmt_file = file_names[0], seq_file = file_names[1], channel_file = file_names[2]))
-                            except AssertionError:
-                                self.fail("PlugExperiment raised an AssertionError even though it was supplied with proper file names")
+    def setUp(self):
+
+        self.tmpdir = tempfile.mkdtemp()
+
+
+    def tearDown(self):
+
+        shutil.rmtree(self.tmpdir)
+
+
+    def test_check_input_file_names(self):
+        """
+        Tests if input file search is properly implemented.
+        """
+
+        fnames = (
+            ('exp.txt', 'channels.tsv', 'sequence_879.csv'),
+            ('Experiment_42.txt.gz', 'CHANNELS-xy.CSV', None),
+            ('exp.xlsx', 'channel_layout.csv', 'Seq029.tsv'),
+            ('fluorescence.txt', 'channel_layout.csv', 'Seq029.tsv'),
+            ('fluor.TXT.gz', 'CHANNELS-xy.CSV', 'seq.tsv'),
+        )
+
+        attrs = ('pmt', 'channel', 'seq')
+
+        for test_fnames in fnames:
+
+            for fn in test_fnames:
+
+                if fn is None:
+
+                    continue
+
+                path = pl.Path(self.tmpdir, fn)
+                path.touch()
+
+            if test_fnames[0].endswith('xlsx'):
+
+                self.assertRaises(
+                    FileNotFoundError,
+                    lambda: PlugyConfig(input_dir = self.tmpdir)
+                )
+
+            else:
+
+                cfg = PlugyConfig(input_dir = self.tmpdir)
+
+                for fn, attr in zip(test_fnames, attrs):
+
+                    attr = '%s_file' % attr
+                    path = (
+                        None
+                            if fn is None else
+                        pl.Path(self.tmpdir, fn)
+                    )
+
+                    self.assertEqual(getattr(cfg, attr), path)
+
+            for f in os.listdir(self.tmpdir):
+
+                os.remove(os.path.join(self.tmpdir, f))
 
 
 if __name__ == '__main__':
+
     unittest.main()
