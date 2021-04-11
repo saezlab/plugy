f3e434aeea0a3fb30f2a4e1d0c458a9f7cd246fb
Türei Dénes <turei.denes@gmail.com>
2021-04-11 23:12:32 +0200

    version 0.7.0, added Olga to the authors

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='plugy',
-    version='v0.6.0',
+    version='0.7.0',
     packages=['plugy', 'plugy.data', 'plugy.test'],
     url='https://github.com/saezlab/plugy',
     license='GPLv3',
-    author='Dénes Türei & Nicolas Peschke',
+    author='Dénes Türei, Nicolas Peschke, Olga Ivanova',
     author_email='turei.denes@gmail.com',
-    description='A package to handle plug microfluidics data',
+    description='Processing plug microfluidics data',
     install_requires=[
         'seaborn',
         'matplotlib',
