3bcdacdef9f274864db801d64ba775ed26a40422
Türei Dénes <turei.denes@gmail.com>
2021-10-25 01:15:23 +0200

    v1.0.7 version bump (minor adjustments, see previous commits)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('requirements.txt', 'r') as fp:
 
 setup(
     name = 'plugy',
-    version = '1.0.6',
+    version = '1.0.7',
     packages = ['plugy', 'plugy.data', 'plugy.test'],
     url = 'https://github.com/saezlab/plugy',
     license = 'GPLv3',
